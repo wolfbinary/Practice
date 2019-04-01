fb2e933d703cc44d891d9fc603dd7d43a3c4c527
Cameron Stoner <wolfbinary@gmail.com>
2019-04-01 16:35:50 -0500

    Finished Module 4

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -41,7 +41,16 @@ export default {
       return this.parts[this.selectedPartIndex];
     },
   },
+  created() {
+    this.emitSelectedPart();
+  },
+  updated() {
+    this.emitSelectedPart();
+  },
   methods: {
+    emitSelectedPart() {
+      this.$emit('partSelected', this.selectedPart);
+    },
     selectNextPart() {
       this.selectedPartIndex = getNextValidIndex(
         this.selectedPartIndex,
--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -1,25 +1,48 @@
 <template>
   <div class="content">
-    <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    <div class="preview">
+      <CollapsibleSection>
+        <div class="preview-content">
+          <div class="top-row">
+            <img :src="selectedRobot.head.src"/>
+          </div>
+          <div class="middle-row">
+            <img :src="selectedRobot.leftArm.src" class="rotate-left"/>
+            <img :src="selectedRobot.torso.src"/>
+            <img :src="selectedRobot.rightArm.src" class="rotate-right"/>
+          </div>
+          <div class="bottom-row">
+            <img :src="selectedRobot.base.src"/>
+          </div>
+        </div>
+      </CollapsibleSection>
+      <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
+    </div>
+
     <div class="top-row">
         <!-- <div class="robot-name">
           {{selectedRobot.head.title}}
           <span v-if="selectedRobot.head.onSale" class="sale">Sale!</span>
           </div> -->
       <PartSelector :parts="availableParts.heads"
-      position="top"/>
+      position="top"
+      @partSelected="part=> selectedRobot.head=part"/>
     </div>
     <div class="middle-row">
       <PartSelector :parts="availableParts.arms"
-      position="left"/>
+      position="left"
+      @partSelected="part=> selectedRobot.leftArm=part"/>
       <PartSelector :parts="availableParts.torsos"
-      position="center"/>
+      position="center"
+      @partSelected="part=> selectedRobot.torso=part"/>
       <PartSelector :parts="availableParts.arms"
-      position="right"/>
+      position="right"
+      @partSelected="part=> selectedRobot.rightArm=part"/>
     </div>
     <div class="bottom-row">
       <PartSelector :parts="availableParts.bases"
-      position="bottom"/>
+      position="bottom"
+      @partSelected="part=> selectedRobot.base=part"/>
     </div>
     <div>
       <h1>Cart</h1>
@@ -46,10 +69,11 @@
 import availableParts from '../data/parts';
 import createdHookMixin from './created-hook-mixin';
 import PartSelector from './PartSelector.vue';
+import CollapsibleSection from '../shared/CollapsibleSection.vue';
 
 export default {
   name: 'RobotBuilder',
-  components: { PartSelector },
+  components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
@@ -195,8 +219,7 @@ width:100%;
 }
 .add-to-cart{
   position:absolute;
-  right:30px;
-  width:220px;
+  width:210px;
   padding: 3px;
   font-size:16px;
 }
@@ -210,6 +233,31 @@ td,th{
 }
 .sale-border{
 border: 3px solid red;
+}
+.preview {
+    position: absolute;
+    top: -20px;
+    right: 0;
+    width: 210px;
+    height: 210px;
+    padding: 5px;
+}
+
+.preview-content {
+    border: 1px solid #999;
+}
+
+.preview img {
+    width: 50px;
+    height: 50px;
+}
+
+.rotate-right {
+    transform: rotate(90deg);
+}
+
+.rotate-left {
+    transform: rotate(-90deg);
 }
 
 </style>
