8abdb2d8aca5230828fae689530916c895ffada3
Cameron Stoner <wolfbinary@gmail.com>
2019-04-19 11:10:06 -0500

    using named views

--- build-a-bot/src/App.vue
+++ build-a-bot/src/App.vue
@@ -17,9 +17,14 @@
       </ul>
     </nav>
   </header>
+  <div class="container">
+    <aside class="aside">
+      <router-view name="sidebar"/>
+    </aside>
     <main>
         <router-view/>
     </main>
+  </div>
   </div>
 </template>
 
@@ -41,15 +46,14 @@ body{
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
 }
 main{
-  margin:0 auto;
   padding:30px;
   background-color: white;
-  width:1024px;
+  width:964px;
   min-height:300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -70,10 +74,19 @@ ul {
   text-decoration:none;
   color:inherit;
 }
-
 .router-link-active{
   color:white;
 }
-
+.container{
+  display: flex;
+  margin: 10px auto 0 auto;
+  justify-content:center;
+}
+.aside{
+  padding:30px;
+  background-color:#aaa;
+  width:100px;
+  min-height:300px;
+}
 
 </style>
